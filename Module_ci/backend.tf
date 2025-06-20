c4e629e5216282640f99367da82605bf22be0ca0
AniruddhDangarh <aniruddh.dangarh.snaatak@mygurukulam.co>
2025-06-20 16:54:15 +0530

    Update backend.tf

--- Module_ci/backend.tf
+++ Module_ci/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "mybackup20062025"
     region         = "ap-northeast-1"
-    dynamodb_table = "myDynamo20062025"
     encrypt        = true
     key  =  "terraform.tfstate"
   }
