1f6d31fc4c6ad83ffee015d4aaf3f38195aff539
AniruddhDangarh <aniruddh.dangarh.snaatak@mygurukulam.co>
2025-06-29 12:26:58 +0530

    Update variables.tf

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,1 +1,15 @@
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for EC2 instance"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type for EC2"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Tag name for EC2 instance"
+}
 
