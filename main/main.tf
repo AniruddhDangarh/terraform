b4d20090d956724b4a394bc6049fe4e99550b439
AniruddhDangarh <aniruddh.dangarh.snaatak@mygurukulam.co>
2025-06-29 12:27:49 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "ec2_instance" {
+  source         = "../modules/ec2_instance"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  instance_name  = var.instance_name
+}
 
